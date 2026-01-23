9f572177cb9e63c05b0e9be866336022a2995e7f
Merwini <86023141+Merwini@users.noreply.github.com>
2026-01-23 01:09:17 -0500

    Eruption ritual

---
+++ Source/TheSecretOfAnimaRituals/PsychicRituals/PsychicRitualDef_Eruption.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI.Group;
+using RimWorld;
+
+namespace tsoa.rituals
+{
+    public class PsychicRitualDef_Eruption : PsychicRitualDef_Unlocked
+    {
+        public SimpleCurve successCurve;
+        public SimpleCurve successCurveAnyOre;
+        public string anyOreOptionString;
+
+        private ThingDef chosenOreDef;
+
+        public override List<string> FloatMenuOptionStrings
+        {
+            get
+            {
+                List<string> options = new List<string>();
+                options.Add(anyOreOptionString);
+
+                //TODO if (researchProjectDef.IsFinished)
+                if (true)
+                {
+                    foreach (var ore in DefDatabase<ThingDef>.AllDefs.Where(td => td.building != null && td.building.isResourceRock))
+                    {
+                        options.Add(ore.label);
+                    }
+                }
+
+                return options;
+            }
+        }
+
+        public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph graph)
+        {
+            List<PsychicRitualToil> list = base.CreateToils(psychicRitual, graph);
+            list.Add(new PsychicRitualToil_Eruption(InvokerRole, chosenOreDef));
+            return list;
+        }
+
+        public override TaggedString OutcomeDescription(FloatRange qualityRange, string qualityNumber, PsychicRitualRoleAssignments assignments)
+        {
+            if (chosenOreDef == null) // i.e. "any ore" was selected
+            {
+                return outcomeDescription.Formatted(successCurveAnyOre.Evaluate(qualityRange.min).ToStringPercent());
+            }
+            return outcomeDescription.Formatted(successCurve.Evaluate(qualityRange.min).ToStringPercent());
+        }
+
+        public override void InitializeCast(Map map)
+        {
+            base.InitializeCast(map);
+
+            chosenOreDef = ResolveOreDef(selectedFloatMenuOptionString);
+        }
+
+        private ThingDef ResolveOreDef(string chosen)
+        {
+            return DefDatabase<ThingDef>.AllDefs.FirstOrDefault(td => td.label == chosen);
+        }
+    }
+}
---
+++ Source/TheSecretOfAnimaRituals/PsychicRituals/PsychicRitualToil_Eruption.cs
@@ -0,0 +1,144 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using Verse;
+using Verse.AI.Group;
+
+namespace tsoa.rituals
+{
+    public class PsychicRitualToil_Eruption : PsychicRitualToil
+    {
+        private PsychicRitualRoleDef invokerRole;
+        private ThingDef chosenOreDef;
+
+        public PsychicRitualToil_Eruption()
+        {
+        }
+
+        public PsychicRitualToil_Eruption(PsychicRitualRoleDef invokerRole, ThingDef chosenOreDef)
+        {
+            this.invokerRole = invokerRole;
+            this.chosenOreDef = chosenOreDef;
+        }
+
+        public override void Start(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+        {
+            base.Start(psychicRitual, parent);
+
+            Pawn invoker = psychicRitual.assignments.FirstAssignedPawn(invokerRole);
+
+            bool success = chosenOreDef == null
+                ? Rand.Chance(((PsychicRitualDef_Eruption)psychicRitual.def).successCurveAnyOre.Evaluate(psychicRitual.PowerPercent))
+                : Rand.Chance(((PsychicRitualDef_Eruption)psychicRitual.def).successCurve.Evaluate(psychicRitual.PowerPercent));
+
+            psychicRitual.ReleaseAllPawnsAndBuildings();
+
+            if (invoker != null)
+            {
+                ApplyOutcome(psychicRitual, invoker, success);
+            }
+        }
+
+        private void ApplyOutcome(PsychicRitual psychicRitual, Pawn invoker, bool success)
+        {
+            if (chosenOreDef == null)
+            {
+                List<ThingDef> potentialOres = DefDatabase<ThingDef>.AllDefs.Where(td => td.building != null && td.building.isResourceRock).ToList();
+                chosenOreDef = potentialOres.RandomElementByWeight(td => td.building.mineableScatterCommonality);
+            }
+
+            int oreAmount = chosenOreDef.building.mineableScatterLumpSizeRange.RandomInRange;
+            int oreSqrt = Mathf.RoundToInt((float)Math.Sqrt(oreAmount));
+
+            Map map = invoker.Map;
+
+            IntVec3 chosenCell = ChooseCell(map, oreSqrt + 1);
+
+            List<Thing> spawnedOres = SpawnOreVein(oreAmount, map, chosenCell);
+            SpawnFilthAround(spawnedOres);
+
+            LetterDef textLetterDef;
+            TaggedString text;
+
+            if (!success)
+            {
+                textLetterDef = LetterDefOf.NegativeEvent;
+                text = "TSOA_EruptionFailure".Translate(invoker, psychicRitual.def.Named("RITUAL"));
+                SpawnInfestation(invoker, map, chosenCell);
+            }
+            else
+            {
+                textLetterDef = LetterDefOf.PositiveEvent;
+                text = "TSOA_EruptionSuccess".Translate(invoker, psychicRitual.def.Named("RITUAL"));
+            }
+
+            Find.LetterStack.ReceiveLetter("PsychicRitualCompleteLabel".Translate(psychicRitual.def.label), text, textLetterDef, lookTargets: new LookTargets(chosenCell, map));
+        }
+
+        private void SpawnInfestation(Pawn invoker, Map map, IntVec3 chosenCell)
+        {
+            IntVec3 intVec = CellFinder.FindNoWipeSpawnLocNear(chosenCell, map, ThingDefOf.TunnelHiveSpawner, Rot4.North, 2, (IntVec3 x) => x.Walkable(map) && x.GetFirstThing(map, chosenOreDef) == null && x.GetFirstThing(map, ThingDefOf.Hive) == null && x.GetFirstThing(map, ThingDefOf.TunnelHiveSpawner) == null);
+            TunnelHiveSpawner tunnelHiveSpawner = (TunnelHiveSpawner)ThingMaker.MakeThing(ThingDefOf.TunnelHiveSpawner);
+            tunnelHiveSpawner.spawnHive = false;
+            tunnelHiveSpawner.insectsPoints = Mathf.Clamp(StorytellerUtility.DefaultThreatPointsNow(invoker.Map) * Rand.Range(0.3f, 0.6f), 200f, 1000f);
+            tunnelHiveSpawner.spawnedByInfestationThingComp = true;
+            GenSpawn.Spawn(tunnelHiveSpawner, intVec, map, WipeMode.FullRefund);
+        }
+
+        private List<Thing> SpawnOreVein(int oreAmount, Map map, IntVec3 chosenCell)
+        {
+            List<Thing> spawnedOres = new List<Thing>();
+            foreach (IntVec3 cell in GridShapeMaker.IrregularLump(chosenCell, map, oreAmount, Validator))
+            {
+                spawnedOres.Add(GenSpawn.Spawn(chosenOreDef, cell, map));
+            }
+            return spawnedOres;
+        }
+
+        private void SpawnFilthAround(List<Thing> spawnedOres)
+        {
+            foreach (Thing ore in spawnedOres)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    IntVec3 adjacentCell = ore.Position + GenAdj.CardinalDirections[i];
+                    if (adjacentCell.InBounds(ore.Map) && Rand.Chance(0.5f))
+                    {
+                        FilthMaker.TryMakeFilth(adjacentCell, ore.Map, ThingDefOf.Filth_RubbleRock);
+                    }
+                }
+            }
+        }
+
+        private IntVec3 ChooseCell(Map map, int size)
+        {
+            IntVec3 randomCell = CellFinder.RandomCell(map); 
+            if (DropCellFinder.TryFindDropSpotNear(randomCell, map, out IntVec3 result, true, true, false, new IntVec2(size, size), false))
+            {
+                return result;
+            }
+            else
+            {
+                return randomCell;
+            }
+        }
+
+        bool Validator(IntVec3 cell)
+        {
+            // TODO
+            return true;
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref invokerRole, "invokerRole");
+            Scribe_Defs.Look(ref chosenOreDef, "chosenOreDef");
+        }
+    }
+}
