e2212997dc14dd4aa71248aa5bbae1d20f919a68
Nuff <me.megalomatt@gmail.com>
2026-03-16 23:58:12 -0400

    Animagenesis WIP

--- Source/TheSecretOfAnimaRituals/PsychicRituals/PsychicRitualDef_Animagenesis.cs
+++ Source/TheSecretOfAnimaRituals/PsychicRituals/PsychicRitualDef_Animagenesis.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
-using RimWorld;
+using Verse.AI.Group;
 
 namespace tsoa.rituals;
 
 public class PsychicRitualDef_Animagenesis : PsychicRitualDef_Unlocked
 {
+    public SimpleCurve successCurve;
 
+    public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+    {
+        List<PsychicRitualToil> list = base.CreateToils(psychicRitual, parent);
+        list.Add(new PsychicRitualToil_Animagenesis(InvokerRole, TargetRole));
+        //list.Add(new PsychicRitualToil_TargetCleanup(InvokerRole, TargetRole));
+        return list;
+    }
+
+    public override TaggedString OutcomeDescription(FloatRange qualityRange, string qualityNumber, PsychicRitualRoleAssignments assignments)
+    {
+        return outcomeDescription.Formatted().ToString();
+    }
 }
---
+++ Source/TheSecretOfAnimaRituals/PsychicRituals/PsychicRitualToil_Animagenesis.cs
@@ -0,0 +1,56 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI.Group;
+using static UnityEngine.GraphicsBuffer;
+
+namespace tsoa.rituals;
+
+public class PsychicRitualToil_Animagenesis : PsychicRitualToil_AnimaAffinity
+{
+    PsychicRitualRoleDef invokerRole;
+    PsychicRitualRoleDef targetRole;
+
+    public PsychicRitualToil_Animagenesis()
+    {
+    }
+
+    public PsychicRitualToil_Animagenesis(PsychicRitualRoleDef invokerRole, PsychicRitualRoleDef targetRole)
+    {
+        this.invokerRole = invokerRole;
+        this.targetRole = targetRole;
+    }
+
+    public override void Start(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+    {
+        base.Start(psychicRitual, parent);
+
+        bool success = Rand.Chance(((PsychicRitualDef_Animagenesis)psychicRitual.def).successCurve.Evaluate(psychicRitual.PowerPercent));
+
+        Pawn invoker = psychicRitual.assignments.FirstAssignedPawn(invokerRole);
+        Pawn target = psychicRitual.assignments.FirstAssignedPawn(targetRole);
+
+        if (invoker != null && target != null)
+        {
+            ApplyOutcome(psychicRitual, success, invoker, target);
+        }
+    }
+
+    private void ApplyOutcome()
+    {
+        // kill the target
+        // apply psychic soothe if successful
+        // spawn an anima seed/pearl (need texture)
+    }
+
+    public override void ExposeData()
+    {
+        base.ExposeData();
+        Scribe_Defs.Look(ref invokerRole, "invokerRole");
+        Scribe_Defs.Look(ref targetRole, "targetRole");
+    }
+}
